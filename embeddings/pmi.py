366abd127f592803c7ae6fa6b83d17fb03c246ce
Makar Fedorov <70643747+makarfedorov@users.noreply.github.com>
2022-05-13 17:38:17 +0300

    Create pmi.py

---
+++ embeddings/pmi.py
@@ -0,0 +1,12 @@
+def pmi(df, positive=True):
+    col_totals = df.sum(axis=0)
+    total = col_totals.sum()
+    row_totals = df.sum(axis=1)
+    expected = np.outer(row_totals, col_totals) / total
+    df = df / expected
+    with np.errstate(divide='ignore'):
+        df = np.log(df)
+    df[np.isinf(df)] = 0.0  # log(0) = 0
+    if positive:
+        df[df < 0] = 0.0
+    return df
